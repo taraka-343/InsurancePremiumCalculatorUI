a9d92c8886ff0eaeab5fc5913e39a87561388727
taraka-343 <usivakumar343@gmail.com>
2025-11-17 22:07:10 +0530

    adding service file and importing HttpClientModule

--- src/app/app-service.service.ts
+++ src/app/app-service.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppServiceService {
+  private apiUrl = "https://localhost:7065/api/Premium/calculate"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
